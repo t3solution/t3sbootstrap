t3solution/t3sbootstrap

commit 5aca9cb496080fb0b5e16698ecdfe96864e2710f

Declaring reinstalled tasks
... "CdnToLocal" && "Scss" for TYPO3 v9

ext_localconf.php
@@ -255,6 +255,14 @@ call_user_func(function () {
 	if ( \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('ws_scss') ) {
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('bootstrap.ext.wsScss = 1');
 		if (array_key_exists('customScss', $extconf) && $extconf['customScss'] === '1') {
+			if ($version == 9) {
+				# declaring the task to write a custom scss file
+				$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['T3SBS\T3sbootstrap\Tasks\Scss'] = [
+						 'extension' => 't3sbootstrap',
+						 'title' => 'T3SB Custom Scss - write a custom scss file',
+						 'description' => 'T3SB Custom Scss - write a custom scss file',
+				];
+			}			
 			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants('bootstrap.extconf.customScss = 1');
 			// Optional "bootswatch theme"
 			if (array_key_exists('bootswatch', $extconf) && $extconf['bootswatch'] !== 'none') {
@@ -312,5 +320,15 @@ call_user_func(function () {
 	if($rootlinefields != '') $rootlinefields .= ' , ';
 	$rootlinefields .= 'keywords,description';
 
+	if ($version == 9) {
+		/***************
+		 * declaring the task to write required CSS and JS files to fileadmin/Resources/Private/
+		 */
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['T3SBS\T3sbootstrap\Tasks\CdnToLocal'] = [
+			 'extension' => 't3sbootstrap',
+			 'title' => 'T3SB CDN to local',
+			 'description' => 'write required CSS and JS to fileadmin/Resources/Private/',
+		];
+	}	
 
 });

commit d5d54697eb1201021a30ad84d21f3bc78374b706

BUGFIX: Move version variable in outer scope
Replace getVersion() with getMajorVersion().
Move $version outside if-scope, because it is used later in the function.
Fixes the bug introduced in 5aca9cb496080fb0b5e16698ecdfe96864e2710f.

ext_localconf.php
@@ -70,6 +70,9 @@ call_user_func(function () {
 	 */
 	$extconf = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)->get('t3sbootstrap');
 
+	$typo3Version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+	$version = $typo3Version->getMajorVersion();
+
 	/***************
 	 * Other Extensions
 	 */
@@ -79,9 +82,6 @@ call_user_func(function () {
 		/***************
 		 * plugin content consent
 		 */
-
-		$typo3Version = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
-		$version = (int)$typo3Version->getVersion();
 
 		if ($version == 10) {
 