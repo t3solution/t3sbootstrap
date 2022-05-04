d36a713fe3c192fc0b135c1dabd2426282261573
root <root@localhost.localdomain>
2022-05-04 15:21:46 +0000

    initial db amendments

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,6 +2,23 @@
 # Table structure for table 'tx_t3sbootstrap_domain_model_config'
 #
 CREATE TABLE tx_t3sbootstrap_domain_model_config (
+	cmi_enabled tinyint(1) unsigned DEFAULT '0' NOT NULL,
+	cmi_settings varchar(100) DEFAULT '' NOT NULL,
+	cmi_objprop_lang varchar(15) DEFAULT '' NOT NULL,
+	cmi_datamodel_course_id varchar(255) DEFAULT '' NOT NULL,
+	cmi_datamodel_course_title varchar(100) DEFAULT '' NOT NULL,
+	cmi_datamodel_course_descr varchar(255) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_id varchar(255) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_title varchar(100) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_descr varchar(255) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_objectives varchar(255) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_acttype varchar(100) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_launchmethod varchar(15) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_moveon varchar(15) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_masteryscore varchar(4) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_url varchar(100) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_launchparms varchar(100) DEFAULT '' NOT NULL,
+	cmi_datamodel_au_entitlementkey varchar(100) DEFAULT '' NOT NULL,
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	deleted tinyint(3) unsigned DEFAULT '0' NOT NULL,
@@ -175,6 +192,10 @@ CREATE TABLE tt_content (
 	tx_t3sbootstrap_animateCssDelay varchar(2) DEFAULT '' NOT NULL,
 	tx_t3sbootstrap_extra_style varchar(100) DEFAULT '' NOT NULL,
 	tx_t3sbootstrap_image_orig varchar(4) DEFAULT '' NOT NULL,
+	###### cmi5 ######
+	tx_t3sbootstrap_cmi5_select_statement varchar(100) DEFAULT '' NOT NULL,
+	tx_t3sbootstrap_cmi5_send_statement tinyint(1) unsigned DEFAULT '0' NOT NULL,
+	###### cmi5 ######
 );
 
 #
@@ -198,6 +219,7 @@ CREATE TABLE sys_file_reference (
 # Table structure for table 'pages'
 #
 CREATE TABLE pages (
+	tx_t3sbootstrap_cmi_generatexml tinyint(1) unsigned DEFAULT '0' NOT NULL,
 	tx_t3sbootstrap_container varchar(255) DEFAULT '' NOT NULL,
 	tx_t3sbootstrap_linkToTop tinyint(4) DEFAULT '0' NOT NULL,
 	tx_t3sbootstrap_smallColumns varchar(1) DEFAULT '' NOT NULL,
