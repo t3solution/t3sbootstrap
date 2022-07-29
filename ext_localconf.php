8874fea20668b6a19a262441f2b7e461e912572d
Helmut Hackbarth <helmuthackbarth@MacBook-Air-M1.fritz.box>
2022-07-29 20:03:41 +0200

    New release v5.1.19

--- ext_localconf.php
+++ ext_localconf.php
@@ -303,4 +303,5 @@ defined('TYPO3') || die();
 		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][PreProcessHook::class]
 		 = PreProcessHook::class . '->execute';
 	}
+
 })();
