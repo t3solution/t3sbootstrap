c50f4af5844b6c7987dc1ed1638bdd58932ec5a7
Robert Kraemer <rkraemer@krplus.de>
2022-10-09 07:21:47 +0000

    bugfix

--- public/typo3/sysext/backend/ext_tables.sql
+++
@@ -1,6 +0,0 @@
-#
-# Table structure for table 'be_users'
-#
-CREATE TABLE be_users (
-	password_reset_token varchar(100) DEFAULT '' NOT NULL
-);
--- public/typo3/sysext/core/Configuration/TCA/Overrides/sys_file_storage.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-defined('TYPO3') or die();
-
-\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\Driver\DriverRegistry::class)->addDriversToTCA();
